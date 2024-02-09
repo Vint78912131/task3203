ecf29ffe345c8a483336fc8d2994456e53902ac2
v sotnikov <sotnikov@rosplatforma.ru>
2024-02-09 13:03:33 +0300

    09022024commit

---
+++ src/Solution.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+
+/*
+Читаем из потока
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("src/testFile.log"));
+        System.out.println(writer.toString());
+    }
+
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        StringWriter writer = new StringWriter();
+        if (is != null) {
+            char[] buffer = new char[1024];
+            try (Reader reader = new BufferedReader(new InputStreamReader(is))) {
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            }
+        }
+        return writer;
+    }
+}
